70652b3df280100f796d138d9947c9f499dd57e3
Dominik Harmim <dominik.harmim@oracle.com>
2025-05-25 09:26:02 +0200

    Initial commit

---
+++ tests/test_model_accuracy.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+# Add the project root directory to the system path for all tests
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from model_training import train_model
+
+
+def test_model_accuracy():
+	_, accuracy = train_model()
+
+	assert accuracy > .8
